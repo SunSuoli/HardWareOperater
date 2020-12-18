6e47655e92b24d88d969a71eb7b125bbe3a160ea
P9PAD0RV4Q2S29P\Administrator <630746464@QQ.COM>
2020-12-18 16:13:24 +0800

    添加项目文件。

---
+++ BinDing.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace HardWareOperater
+{
+    class BinDing
+    {
+        public class Source : INotifyPropertyChanged
+        {
+            public event PropertyChangedEventHandler PropertyChanged;
+            //创建String类型数据源
+            private string data_string;
+            public string Data_String
+            {
+                get { return data_string; }
+                set
+                {
+                    data_string = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("Data_String"));
+                    }
+                }
+            }
+            //创建Int类型数据源
+            private string data_int;
+            public string Data_Int
+            {
+                get { return data_int; }
+                set
+                {
+                    data_int = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("Data_Int"));
+                    }
+                }
+            }
+            //创建bool类型数据源
+            private bool data_bool;
+            public bool Data_bool
+            {
+                get { return data_bool; }
+                set
+                {
+                    data_bool = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("Data_bool"));
+                    }
+                }
+            }
+            //创建Object类型数据源
+            private object data_object;
+            public object Data_object
+            {
+                get { return data_object; }
+                set
+                {
+                    data_object = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("Data_object"));
+                    }
+                }
+            }
+        }
+
+        public static void Bind(Source Data_Source, DependencyObject Control_Name, DependencyProperty Property,String Data_Type)
+        {
+            Binding Bind = new Binding();//实例化绑定
+            Bind.Source = Data_Source;//设置绑定数据源
+            Bind.Path= new PropertyPath("Data_"+Data_Type);//设置绑定路径
+            Bind.Mode = BindingMode.TwoWay;//双向绑定
+            Bind.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;//数据随控件改变
+            BindingOperations.SetBinding(Control_Name, Property, Bind);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using NationalInstruments.DAQmx;
+using System.Collections;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using static HardWareOperater.BinDing;
+
+namespace HardWareOperater
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Task DI_Task = new Task();//一个数字输入任务
+        DigitalMultiChannelReader DigitalReader = null;//定义一个多通道数据流
+        string[] DI_Lines = {
+            "离散输入/port0", 
+            "离散输入/port1",
+            "离散输入/port2", 
+            "离散输入/port3", 
+            "离散输入/port4", 
+            "离散输入/port5", 
+            "离散输入/port6", 
+            "离散输入/port7"};//通道数组常量
+
+        Queue q = new Queue();//操作队列
+
+        Source source = new Source();//绑定数据源
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            /*控件操作*/
+            Bind(source, bool0_0, RadioButton.IsCheckedProperty, "object");//绑定RadioButton
+
+            /*硬件操作*/
+            foreach (string DI_Line in DI_Lines)
+            {
+                DI_Task.DIChannels.CreateChannel(DI_Line,null, ChannelLineGrouping.OneChannelForAllLines);
+            }//创建多个通道
+            DI_Task.Start(); //开始任务/
+            DigitalReader = new DigitalMultiChannelReader(DI_Task.Stream);//实例化数据流（LabVIEW中将这一步和数据读取集成到一起了）
+
+            /*启动状态机线程*/
+            Thread th_main = new Thread(new ThreadStart(Main));
+            th_main.Start();
+
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            q.Enqueue("start");
+        }//开始按钮
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            q.Enqueue("stop");
+        }//停止按钮
+        private void Quiet(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            q.Enqueue("quiet");
+        }//关闭窗体
+        private void Main()
+        {
+            /*状态机的两个关键变量*/
+            string state="";
+            bool run = true;
+            /*需要传输的数据*/
+            bool[,] data;
+            /*主循环*/
+            while (run)
+            {
+                if (q.Count > 0)//通过队列内容改变状态机的状态
+                {
+                    state = (string)q.Dequeue();
+                }
+                else//状态机主体
+                {
+                    switch (state)
+                    {
+                        case "start":
+                           data= DigitalReader.ReadSingleSampleMultiLine();//多通道（通过实例化数据流已经完成设置），单采集，多线
+                            source.Data_object = data[7, 2];
+                            break;
+                        case "stop":
+                            break;
+                        case "quiet":
+                            DI_Task.Stop();
+                            run = false;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                Thread.Sleep(100);//休眠100ms
+            }
+        }
+
+
+        
+    }
+}
